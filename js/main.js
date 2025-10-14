97f86ea41ca20d893e5de1682f061cb59191d8aa
CryptoGot <145673531+CryptoGot@users.noreply.github.com>
2025-10-14 13:35:47 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// (pour l’instant vide) – on branchera plus tard :
+// - analytics éventuels
+// - effets mineurs
+// - et surtout l’intégration du mini-jeu (game.js) quand on passera à l’étape suivante.
+console.log("Portfolio base OK");
